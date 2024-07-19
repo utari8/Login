ddcced0a1b962e52b8f7d9335ce34cbbea044aa8
F. N. Utari <77846876+utari8@users.noreply.github.com>
2024-07-19 21:13:30 +0700

    Add files via upload

--- proses_login.php
+++ proses_login.php
@@ -5,26 +5,24 @@ session_start();
 include("config.php"); 
 
 $username = $_POST['username']; 
-
 $password = $_POST['password']; 
 
-$sql = "SELECT * FROM member WHERE username='$username' AND password='$password'"; 
+$sql = "SELECT password FROM member WHERE username = (?)"; 
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $username);
 
-$result = $conn->query($sql); 
+$stmt->execute();
+$stmt->store_result();
 
-if ($result->num_rows > 0) { 
+if($stmt->num_rows()){
+    $stmt->bind_result($hashed);
+    $stmt->fetch();
 
- $_SESSION['username'] = $username; 
-
- header("Location: welcome.php"); 
-
-} else { 
-
- echo "Login gagal. <a href='index.php'>Coba lagi</a>"; 
-
-} 
-
-$conn->close(); 
-
-
+    if(password_verify($password, $hashed)){
+        header("Location: a.php");
+    } else{
+        echo "Invalid password";
+    }}
+    $stmt->free_result();
+    $stmt->close();
 ?>
